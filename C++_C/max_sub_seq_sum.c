553f26d2bd759de43c5dcd5dfbc0fba9ed221109
peppone zuo <zuopengpeng0123@gmail.com>
2012-10-04 15:26:52 +0800

    finish max_sub_seq_sum

--- C++_C/max_sub_seq_sum.c
+++ C++_C/max_sub_seq_sum.c
@@ -8,7 +8,7 @@ int random_in_range(unsigned int, unsigned int);
 int get_max_subseq_sum1(int *, int);
 int get_max_subseq_sum2(int *, int);
 int get_max_subseq_sum3(int *, int);
-
+int get_max_subseq_sum4(int *, int, int);
 
 int main() 
 {
@@ -16,8 +16,46 @@ int main()
     printf("using algorigthm 1 %d\n", get_max_subseq_sum1(array, 6));
     printf("using algorigthm 2 %d\n", get_max_subseq_sum2(array, 6));
     printf("using algorigthm 3 %d\n", get_max_subseq_sum3(array, 6));
+    printf("using algorigthm 4 %d\n", get_max_subseq_sum4(array, 0, 5));
     //getchar();
     return 0;
+}
+
+int get_max_subseq_sum4(int *A, int left, int right)
+{
+    int ix = 0;
+    int center = 0;
+    int max_sum = 0;
+    int max_right = 0, max_left = 0;
+    int max_border_right = 0, max_border_left = 0;
+    int border_right = 0, border_left = 0;
+    if (left == right)
+        return A[left] > 0 ? A[left] : 0;
+    center = (left + right) / 2;
+    max_right = get_max_subseq_sum4(A, center + 1, right);
+    max_left = get_max_subseq_sum4(A, left, center);
+
+    border_right = 0; max_border_right = 0;
+    for (ix = center + 1; ix <= right; ix++)
+    {
+        border_right += A[ix];
+        if (border_right > max_border_right)
+            max_border_right = border_right;
+    }
+
+    border_left = 0; max_border_left = 0;
+    for (ix = center; ix >= left; ix--)
+    {
+        border_left += A[ix];
+        if (border_left > max_border_left)
+            max_border_left = border_left;
+    }
+    
+    max_sum = max_right > max_left ? max_right : max_left;
+    max_sum = max_sum > (max_border_right + max_border_left) ? max_sum : \
+              (max_border_right + max_border_left);
+
+    return max_sum;
 }
 
 int get_max_subseq_sum3(int *A, int N)
